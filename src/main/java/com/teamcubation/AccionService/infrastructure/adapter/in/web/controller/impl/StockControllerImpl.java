27dd345d43dfd91af5627df5b9615719eaa113ba
MariaPaulaBaldivia <m.paulabaldivia00@gmail.com>
2024-10-14 12:36:35 -0300

    Extraigo strings de logs a constantes en controller

--- src/main/java/com/teamcubation/AccionService/infrastructure/adapter/in/web/controller/impl/StockControllerImpl.java
+++ src/main/java/com/teamcubation/AccionService/infrastructure/adapter/in/web/controller/impl/StockControllerImpl.java
@@ -12,18 +12,26 @@ import com.teamcubation.AccionService.infrastructure.adapter.in.web.controller.m
 import com.teamcubation.AccionService.infrastructure.adapter.in.web.controller.util.validation.ControllerValidation;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.coyote.http11.upgrade.UpgradeProcessorInternal;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Slf4j
 @RequestMapping("/stock")
 public class StockControllerImpl implements StockController {
-
+    private static final String GETTING_ALL_STOCKS = "Getting all stocks";
+    private static final String GETTING_STOCK_BY_ID = "Getting stock by id {}";
+    private static final String STOCK_FOUND_BY_ID = "Stock found by id {}: {}";
+    private static final String CREATING_STOCK = "Creating stock {}";
+    private static final String CREATED_STOCK = "Stock was created: {}";
+    private static final String DELETING_STOCK_BY_ID = "Deleting stock by id {}";
+    private static final String DELETED_STOCK = "Stock with id was deleted: {}";
+    private static final String UPDATING_STOCK_BY_ID = "Updating stock by id {}";
+    private static final String UPDATED_STOCK = "Stock with id {} was updated: {}";
 
     private final StockInPort stockService;
 
@@ -34,7 +42,7 @@ public class StockControllerImpl implements StockController {
     @Override
     @GetMapping("/")
     public ResponseEntity<?> getAll() throws Exception {
-        log.info("Getting all stocks");
+        log.info(GETTING_ALL_STOCKS);
         List<StockResponseDTO> stockResponses = this.getAllStockModelToStockResponseDTO();
         log.info(stockResponses.toString());
         return ResponseEntity.status(HttpStatus.OK).body(stockResponses);
@@ -43,38 +51,38 @@ public class StockControllerImpl implements StockController {
     @Override
     @GetMapping("/{id}")
     public ResponseEntity<?> getById(@PathVariable long id) throws Exception {
-        log.info("Getting stock by id {}", id);
+        log.info(GETTING_STOCK_BY_ID, id);
         StockResponseDTO stockResponseDTO = getByIdStockModelToStockResponseDTO(id);
-        log.info("Stock found by id {}: {}", id,  stockResponseDTO.toString());
+        log.info(STOCK_FOUND_BY_ID, id,  stockResponseDTO.toString());
         return ResponseEntity.status(HttpStatus.OK).body(stockResponseDTO);
     }
 
     @Override
     @PostMapping("/")
     public ResponseEntity<?> create(@Valid @RequestBody StockRequestDTO stockRequestDTO) throws Exception {
-        log.info("Creating stock {}", stockRequestDTO.toString());
+        log.info(CREATING_STOCK, stockRequestDTO.toString());
         ControllerValidation.validateNotNull(stockRequestDTO);
         Stock stockCreated = stockService.create(StockRequestMapper.toStockToCreate(stockRequestDTO));
-        log.info("Stock was created: {}", stockCreated.toString());
+        log.info(CREATED_STOCK, stockCreated.toString());
         return ResponseEntity.status(HttpStatus.CREATED).body(stockCreated);
     }
 
     @Override
     @DeleteMapping("/{id}")
     public ResponseEntity<?> deleteById(@PathVariable long id) throws Exception {
-        log.info("Deleting stock by id {}", id);
+        log.info(DELETING_STOCK_BY_ID, id);
         stockService.deleteById(id);
-        log.info("Stock with id was deleted: {}", id);
+        log.info(DELETED_STOCK, id);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
     @Override
     @PutMapping("/{id}")
     public ResponseEntity<?> update(@PathVariable long id, @RequestBody EditedStockDTO editedStockDTO) throws Exception {
-        log.info("Updating stock by id {}", id);
+        log.info(UPDATING_STOCK_BY_ID, id);
         ControllerValidation.validateNotNull(editedStockDTO);
         StockResponseDTO stockUpdate = StockResponseMapper.toStockResponse(stockService.update(EditedStockMapper.toStockToUpdate(editedStockDTO,id)));
-        log.info("Stock was updated: {}", stockUpdate.toString());
+        log.info(UPDATED_STOCK, id, stockUpdate.toString());
         return ResponseEntity.status(HttpStatus.NO_CONTENT).body(stockUpdate);
     }
 
