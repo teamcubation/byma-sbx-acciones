2fa23e051327ba0734d071e12cbf2f3ca798850b
MariaPaulaBaldivia <m.paulabaldivia00@gmail.com>
2024-10-14 12:46:26 -0300

    Agrego custom exception InvalidStockDTOException para utilizar en el controller

--- src/main/java/com/teamcubation/AccionService/infrastructure/adapter/in/web/controller/util/validation/ControllerValidation.java
+++ src/main/java/com/teamcubation/AccionService/infrastructure/adapter/in/web/controller/util/validation/ControllerValidation.java
@@ -1,13 +1,13 @@
 package com.teamcubation.AccionService.infrastructure.adapter.in.web.controller.util.validation;
 
-import jakarta.validation.ValidationException;
+import com.teamcubation.AccionService.infrastructure.adapter.in.web.controller.exception.InvalidStockDTOException;
 
 public class ControllerValidation {
     public static final String INVALID_OBJECT = "Sent object is null whose class is ";
 
     public static void validateNotNull(Object object) throws Exception {
         if (object == null) {
-            throw new Exception(INVALID_OBJECT + object.getClass().getName());
+            throw new InvalidStockDTOException(INVALID_OBJECT + object.getClass().getName());
         }
     }
 }
