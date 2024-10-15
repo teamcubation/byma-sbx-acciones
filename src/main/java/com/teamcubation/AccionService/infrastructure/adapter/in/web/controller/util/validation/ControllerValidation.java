eb79e1d5684d403a663193ae8086fbd11a537123
MariaPaulaBaldivia <m.paulabaldivia00@gmail.com>
2024-10-14 22:11:27 -0300

    Extraigo mensajes de error en constantes en ControllerValidation

--- src/main/java/com/teamcubation/AccionService/infrastructure/adapter/in/web/controller/util/validation/ControllerValidation.java
+++ src/main/java/com/teamcubation/AccionService/infrastructure/adapter/in/web/controller/util/validation/ControllerValidation.java
@@ -7,29 +7,25 @@ import com.teamcubation.AccionService.infrastructure.adapter.in.web.controller.d
 import com.teamcubation.AccionService.infrastructure.adapter.in.web.controller.exception.InvalidStockDTOException;
 
 public class ControllerValidation {
-    public static final String INVALID_OBJECT = "Sent object is null whose class is ";
-
-    public static void validateNotNull(Object object) throws Exception {
-        if (object == null) {
-            throw new InvalidStockDTOException(INVALID_OBJECT + object.getClass().getName());
-        }
-    }
+    public static final String INVALID_UPDATED_STOCK = "Updated stock sent is null.";
+    public static final String INVALID_STOCK_TO_CREATE = "Stock sent to create is null.";
+    public static final String INVALID_STOCK_MODEL = "Stock model sent is null.";
 
     public static void validateUpdatedStockDTO(UpdatedStockDTO updatedStockDTO) throws InvalidStockDTOException {
         if (ControllerValidation.isNull(updatedStockDTO)) {
-            throw new InvalidStockDTOException("Updated stock sent is null.");
+            throw new InvalidStockDTOException(INVALID_UPDATED_STOCK);
         }
     }
 
     public static void validateStockRequestDTO(StockRequestDTO stockRequestDTO) throws InvalidStockDTOException {
         if (ControllerValidation.isNull(stockRequestDTO)) {
-            throw new InvalidStockDTOException("Stock sent to create is null.");
+            throw new InvalidStockDTOException(INVALID_STOCK_TO_CREATE);
         }
     }
 
     public static void validateStockModel(Stock stock) throws InvalidStockModelException {
         if(ControllerValidation.isNull(stock)) {
-            throw new InvalidStockModelException("Stock model sent is null.");
+            throw new InvalidStockModelException(INVALID_STOCK_MODEL);
         }
     }
 
